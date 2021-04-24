8e3e55459f0620a567ebe29c7e0347a8ed1bb8dc
tatyana-zholobova <44976824+tatyana-zholobova@users.noreply.github.com>
2021-04-24 09:54:51 +0300

    Переделана шапка сайта, подключены скрипты

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,17 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+const header = document.querySelector('.header');
+const siteNavigation = document.querySelector('.header__site-navigation');
+const buttonOpen = document.querySelector('.header__toggle--open');
+const buttonClose = document.querySelector('.header__toggle--close');
 
-pageHeader.classList.remove('page-header--nojs');
+header.classList.remove('header--no-js');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+buttonOpen.addEventListener('click', function () {
+  siteNavigation.classList.remove('header__site-navigation--close');
+  siteNavigation.classList.add('header__site-navigation--open');
+})
+
+buttonClose.addEventListener('click', function () {
+  siteNavigation.classList.remove('header__site-navigation--open');
+  siteNavigation.classList.add('header__site-navigation--close');
+})
