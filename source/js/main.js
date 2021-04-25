a709b437b927a21cb42c6563a4b71c100dd8b2e7
tatyana-zholobova <44976824+tatyana-zholobova@users.noreply.github.com>
2021-04-25 08:38:14 +0300

    Откорректирован по stylelint

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,5 @@
 'use strict';
+const pageBody = document.querySelector('.page__body');
 const header = document.querySelector('.header');
 const siteNavigation = document.querySelector('.header__site-navigation');
 const buttonOpen = document.querySelector('.header__toggle--open');
@@ -9,9 +10,11 @@ header.classList.remove('header--no-js');
 buttonOpen.addEventListener('click', function () {
   siteNavigation.classList.remove('header__site-navigation--close');
   siteNavigation.classList.add('header__site-navigation--open');
-})
+  pageBody.classList.add('page__body--overlay');
+});
 
 buttonClose.addEventListener('click', function () {
   siteNavigation.classList.remove('header__site-navigation--open');
   siteNavigation.classList.add('header__site-navigation--close');
-})
+  pageBody.classList.remove('page__body--overlay');
+});
