9715d017449474df04d6ae741555f18376e86886
tatyana-zholobova <44976824+tatyana-zholobova@users.noreply.github.com>
2021-04-29 14:44:03 +0300

    Исправлены баги

--- build/js/main.js
+++ build/js/main.js
@@ -4,6 +4,8 @@ var header = document.querySelector('.header');
 var siteNavigation = document.querySelector('.header__site-navigation');
 var buttonOpen = document.querySelector('.header__toggle--open');
 var buttonClose = document.querySelector('.header__toggle--close');
+var feedbackForm = document.querySelector('.feedback__form');
+var telephoneInput = feedbackForm.querySelector('#telephone');
 
 header.classList.remove('header--no-js');
 
@@ -17,4 +19,12 @@ buttonClose.addEventListener('click', function () {
   siteNavigation.classList.remove('header__site-navigation--open');
   siteNavigation.classList.add('header__site-navigation--close');
   pageBody.classList.remove('page__body--overlay');
+});
+
+telephoneInput.addEventListener('invalid', function () {
+  if (telephoneInput.validity.patternMismatch) {
+    telephoneInput.setCustomValidity('Введите 11-значный номер телефона');
+  } else {
+    telephoneInput.setCustomValidity('');
+  }
 });
